d543cc64d32f0bed933677e3d30fc5ca337b1266
esolomon <esolo@upenn.edu>
2016-01-13 15:20:39 -0500

    add flag to automatically handle cases where no params file is found.

--- __init__.py
+++ __init__.py
@@ -10,8 +10,17 @@ from os.path import *
 from ptsa.data.events import Events
 from ptsa.data.rawbinwrapper import RawBinWrapper
 
-def get_sub_events(task, sub, eliminate_events_with_no_eeg=True, rhino_prefix='/'):
-    """Returns all events for a given task and subject identifier"""    
+def get_sub_events(task, sub, eliminate_events_with_no_eeg=True, rhino_prefix='/', ignore_no_params=False):
+    """Returns all events for a given task and subject identifier
+
+    Keyword arguments:
+    task -- string of task name
+    sub -- string of subject identifier
+    eliminate_events_with_no_eeg -- (default: True) Do not return events for which no corresponding eeg file exists.
+    rhino_prefix -- (default: '/') To get data if rhino is mounted as an external volume from a local computer.
+    ignore_no_params -- (default: False) Automatically handles IOError when no params.txt file is found for a subject.
+
+    """
     from MatlabIO import read_single_matlab_matrix_as_numpy_structured_array
     
     e_path = rhino_prefix+'/data/events/'+task+'/'+sub+'_events.mat'    
@@ -36,16 +45,32 @@ def get_sub_events(task, sub, eliminate_events_with_no_eeg=True, rhino_prefix='/
 
     import pathlib
 
-    for ev in evs:
+    #This may throw and error for a few subjects who have no params file. Should be written to handle this elegantly.
+
+    if ignore_no_params:
         try:
-            eeg_file_path = join(rhino_prefix, str(pathlib.Path(str(ev.eegfile)).parts[1:]))
-            ev.esrc = RawBinWrapper(eeg_file_path)
-            #self.raw_data_root=str(eeg_file_path)
-        except TypeError:
-            print 'skipping event with eegfile=',ev.eegfile
-            pass
-    
-    return(evs)    
+            for ev in evs:
+                try:
+                    eeg_file_path = join(rhino_prefix, str(pathlib.Path(str(ev.eegfile)).parts[1:]))
+                    ev.esrc = RawBinWrapper(eeg_file_path)
+                    #self.raw_data_root=str(eeg_file_path)
+                except TypeError:
+                    print 'skipping event with eegfile=',ev.eegfile
+                    pass
+        except IOError:
+            print('No params.txt or .params file found for '+sub)
+
+    else:
+        for ev in evs:
+            try:
+                eeg_file_path = join(rhino_prefix, str(pathlib.Path(str(ev.eegfile)).parts[1:]))
+                ev.esrc = RawBinWrapper(eeg_file_path)
+                #self.raw_data_root=str(eeg_file_path)
+            except TypeError:
+                print 'skipping event with eegfile=',ev.eegfile
+                pass
+
+    return(evs)
     
     
 def get_bp_tal_data(sub, rhino_prefix='/'):
@@ -63,3 +88,9 @@ def get_all_subs(taskname, rhino_prefix='/'):
     subs = [s.split('/')[-1].split('_e')[0] for s in fns]
     
     return(subs)
+    
+def get_mtl_leads():
+    pass
+    
+def get_hipp_elecs():
+    pass
